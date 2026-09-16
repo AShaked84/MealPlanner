58536254dbd8452e1d8b03993cb1ed4aa48a2bbc
Adi Shaked <ashaked84@gmail.com>
2026-09-17 02:01:03 +0300

    recipe scraper is now a function because it was annoying in the main file and also because that's really where it should be

--- IngredientLister.py
+++ IngredientLister.py
@@ -1,10 +1,23 @@
 from fractions import Fraction
+from collections import defaultdict
+from recipe_scrapers import scrape_me
+
+
 
 units = [
     "lb", "lb.", "Tbsp", "tsp", "cloves",
     "cup", "cups", "oz", "ounce", "ounces",
     "g", "kg", "ml", "l"
 ]
+
+#scrape recipe from card. Function recieves a url string and returns a json file of all of the data the recipe card has to offer. go wild.
+def recipe_scraper(url):
+    scraper = scrape_me(url)
+    scraper.title()
+    scraper.instructions()
+    json_data = scraper.to_json()
+    return json_data
+    
 
 #function recieves a string, returns a boolean on whether it is a number, includes fractions ("1/4") and decimals ("3.14")
 def is_number_or_fraction(word):
@@ -49,3 +62,13 @@ def groceryList(ingredients, default_servings, required_servings):
         ingredient["amount"] = ingredient["amount"] * serving_factor
 
     return ingredient_dict
+
+def combineLists(dict_list):
+    result = defaultdict(int)
+    for dic in dict_list:
+        for key, value in dic.items():
+            result[key] += value["amount"]
+            print(key, value["amount"])
+
+    final_dict = dict(result)
+    return final_dict
