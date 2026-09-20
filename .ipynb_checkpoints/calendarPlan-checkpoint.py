679088000adb366812520e04ae1af495c8e79447
Adi Shaked <ashaked84@gmail.com>
2026-09-20 17:35:19 +0300

    to meal manager - iterate through the meal plan and count how many servings of each recipe need to be prepared

--- .ipynb_checkpoints/calendarPlan-checkpoint.py
+++ .ipynb_checkpoints/calendarPlan-checkpoint.py
@@ -1,5 +1,8 @@
 from datetime import datetime, timedelta, date
 
+#creates a calendar for this week, starting sunday, with a space to add a recipe for each meal
+#recipe saved as id number
+#output: {datetime.date:{string:int, string:int, string:int}, ...}
 def calendarMaker():
     start_date = date.today()
 
@@ -16,13 +19,15 @@ def calendarMaker():
 
     return meal_plan
 
+#function recieves meal_plan created by calendarMaker, a datetime date (eg date(year, month,  date), the meal to fill out as a string, and the id for the chosen recipe. 
+#the chosen recipe is saved to the chosen meal as its id number
 def addMeal(meal_plan, date, meal, recipe_id):
-    connection = sqlite3.connect("database/movies.db")
+    connection = sqlite3.connect("database/RecipeBook.db")
     cursor = connection.cursor()
-    result = cursor.execute("SELECT * FROM RecipeBook WHERE ID = ?", [recipe_id])
+    result = cursor.execute("SELECT * FROM Recipes WHERE ID = ?", [recipe_id])
     recipe = result.fetchall()
     
-    print(recipe[1])
+    meal_plan[date][meal] = recipe[0]
     
     #how many servings do we have of each 
     
--- MealManager.py
+++ MealManager.py
@@ -1,7 +1,13 @@
-#how many servings do we have of a meal?
-#def servingStocker(recipe_name, servings):
+#once our meal plan is done we can count the instances of each meal, this way we know how many servings to prepare of each recipe.
+#this function recieves the meal plan, then iterates through it to count instances of each recipe. It returns a new dictionary {recipe_id:count}
+def mealCounter(meal_plan):
+    meal_counts = {}
     
+    for day, meals in meal_plan.items():
+        for time, recipe in meals.items():
+            if recipe in meal_counts:
+                meal_counts[recipe] += 1
+            else:
+                meal_counts[recipe] = 1
 
-#add meal to leftovers
-
-#remove serving from leftovers
+    return meal_counts
